2aecdd1376b6ce9de41e4fb6a7a15af11e19f1e9
Doanda Dresta Rahma <66405353+doandadr@users.noreply.github.com>
2026-05-12 04:03:23 +0000

    init commit

---
+++ zetta-test.js
@@ -0,0 +1,40 @@
+/**
+ * Direction:
+ * Find maximum number of words from given sentences.
+ *
+ * Expected:
+ * 23
+ */
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+  'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+  'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+];
+
+function altResult(sentences) {
+    return Math.max(
+        ...sentences.map(sentence =>
+            sentence.trim().split(/\s+/).length
+        )
+    )
+}
+
+function result(sentences) {
+  // iterate sentences
+  // split sentence in to array of words .split(" ")
+  // count array length
+  // find maximum wordCount
+
+  let maxWordCount = 0
+
+  for (let i  = 0; i < sentences.length; i++) {
+    let wordCount = sentences[i].trim().split(/\s+/).length
+    if (maxWordCount < wordCount) {
+        maxWordCount = wordCount
+    }
+  }
+
+  return maxWordCount
+}
+
+console.log(altResult(sentences));
